6032d70dd089abcac90193a5916b4b41ecb60a6c
Kai_WMDE <kai.nissen@wikimedia.de>
2015-05-04 13:37:00 +0200

    refactored processing of flaws parameter for permalinks

--- stools/application/view/Alg/HtmlView.php
+++ stools/application/view/Alg/HtmlView.php
@@ -8,6 +8,14 @@ class Alg_HtmlView extends View {
 	public function getFlaws() {
 		return SingletonFactory::getInstance( 'Alg_Model' )->getFlawList();
 	}
+
+	public function parseFlawsFromQueryString() {
+		$flaws = array();
+		if( isset( $_GET['flaws'] ) ) {
+			$flaws = explode( ' ', urldecode( $_GET['flaws'] ) );
+		}
+		return $flaws;
+	}
 	
 	public function getResults() {
 		return SingletonFactory::getInstance( 'Alg_Model' )->query();
